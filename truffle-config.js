b1de988b254e87cb50022608bcc340f31d09cc5a
Carlos Landeras <carlos.landeras@gmail.com>
2018-10-12 09:12:47 +0200

    Add rinkeby configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+const mnemonic = 'unhappy clarify worth panther sibling sand almost usage pulp fun action toe';
+
 module.exports = {
   networks: {
     development: {      
@@ -5,6 +9,10 @@ module.exports = {
       port: 7545,
       network_id: '*',
       gas: 5000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/LtjZFi6C8XO0qkthtI7p"),
+      network_id: 4
     }
   }
 }
